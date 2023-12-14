70d8bc9daaaa58723e4773a8d0d11013b7f7382e
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-12-14 09:01:05 -0700

    Bugfixes / license property / comments

--- CVSC/CVSC/Program.cs
+++ CVSC/CVSC/Program.cs
@@ -6,39 +6,59 @@ using Newtonsoft.Json.Linq;
 
 class Program
 {
+    // Define valid licenses
+    static string[] validLicenses = {"zlib", "unlicense", "ncsa", "ofl-1.1", "postgresql", "osl-3.0", "mpl-2.0",
+                                     "mit", "ms-pl", "lppl-1.3c", "isc", "lgpl-3.0", "lgpl-2.1", "lgpl", "gpl-3.0",
+                                     "gpl-2.0", "gpl", "agpl-3.0", "eupl-1.1", "epl-2.0", "epl-1.0", "ecl-2.0",
+                                     "wtfpl", "cc-by-sa-4.0", "cc-by-4.0", "cc0-1.0", "cc", "0bsd", "bsd-4-clause",
+                                     "bsd-3-clause-clear", "bsd-3-clause", "bsd-2-clause", "bsl-1.0", "artistic-2.0",
+                                     "apache-2.0", "afl-3.0"};
+
     static void Main(string[] args)
     {
-        if (args.Length < 2)
+        try
         {
-            Console.WriteLine("Usage: cvsc.exe create <project_directory> OR cvsc.exe run <file>");
-            return;
-        }
+            // Check command-line arguments
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: cvsc.exe create <project_directory> OR cvsc.exe run <file>");
+                return;
+            }
 
-        string mode = args[0].ToLower();
+            // Get the mode (create or run)
+            string mode = args[0].ToLower();
 
-        if (mode == "create" && args.Length != 3)
-        {
-            Console.WriteLine("Usage: cvsc.exe create <project_directory>");
-            return;
-        }
-        else if (mode == "run" && args.Length != 2)
-        {
-            Console.WriteLine("Usage: cvsc.exe run <file>");
-            return;
-        }
+            // Validate arguments based on the mode
+            if (mode == "create" && args.Length != 2)
+            {
+                Console.WriteLine("Usage: cvsc.exe create <project_directory>");
+                return;
+            }
+            else if (mode == "run" && args.Length != 2)
+            {
+                Console.WriteLine("Usage: cvsc.exe run <file>");
+                return;
+            }
 
-        if (mode == "create")
-        {
-            string projectDirectory = args[2];
-            CreateProject(projectDirectory);
+            // Execute the appropriate mode
+            if (mode == "create")
+            {
+                string projectDirectory = args[1];
+                CreateProject(projectDirectory);
+            }
+            else if (mode == "run")
+            {
+                string filePath = args[1];
+                RunProject(filePath);
+            }
         }
-        else if (mode == "run")
+        catch (Exception ex)
         {
-            string filePath = args[1];
-            RunProject(filePath);
+            Console.WriteLine($"Error: {ex.Message}");
         }
     }
 
+    // Function to create a CVSCode project
     static void CreateProject(string projectDirectory)
     {
         // Default values
@@ -47,6 +67,7 @@ class Program
         string projectDescription = "Example CVSCode project";
         string projectAuthor = "Your Name";
         string githubRepository = "https://github.com/cvs0/cvscode";
+        string projectLicense = "MIT";
         string projectId = "";
 
         // Prompt the user for project details
@@ -85,6 +106,17 @@ class Program
             githubRepository = input;
         }
 
+        Console.Write("Enter the license type (default: MIT): ");
+        input = Console.ReadLine().Trim();
+        if (!string.IsNullOrEmpty(input) && validLicenses.Contains(input.ToLower()))
+        {
+            projectLicense = input;
+        }
+        else
+        {
+            Console.WriteLine("Invalid license type. Using default: MIT");
+        }
+
         // Create the project directory if it doesn't exist
         if (!Directory.Exists(projectDirectory))
         {
@@ -104,7 +136,8 @@ class Program
             new JProperty("repository", new JObject(
                 new JProperty("type", "git"),
                 new JProperty("url", githubRepository)
-            ))
+            )),
+            new JProperty("license", projectLicense)
         );
 
         File.WriteAllText(packageJsonPath, packageJson.ToString());
@@ -122,6 +155,7 @@ class Program
         Console.WriteLine("CVSCode project created in: " + projectDirectory);
     }
 
+    // Function to run a CVSCode project
     static void RunProject(string filePath)
     {
         string cvsCodeLocalDirectory = @"C:\Program Files\CVSCode\Local";
@@ -133,43 +167,47 @@ class Program
     // Helper function to execute a command in the current directory
     static void ExecuteCommand(string command, string workingDirectory)
     {
-        ProcessStartInfo processStartInfo = new ProcessStartInfo
+        try
         {
-            FileName = "cmd.exe",
-            RedirectStandardInput = true,
-            UseShellExecute = false,
-            RedirectStandardOutput = true,
-            CreateNoWindow = false
-        };
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = false
+            };
 
-        Process process = new Process
+            Process process = new Process
+            {
+                StartInfo = processStartInfo,
+                EnableRaisingEvents = true,
+            };
+
+            // Set the working directory for the process
+            process.StartInfo.WorkingDirectory = workingDirectory;
+
+            process.Start();
+
+            // Send the command to the command prompt
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+
+            // Display the command prompt output
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+
+            process.WaitForExit();
+            process.Close();
+        }
+        catch (Exception ex)
         {
-            StartInfo = processStartInfo,
-            EnableRaisingEvents = true,
-        };
-
-        // Set the working directory for the process
-        process.StartInfo.WorkingDirectory = workingDirectory;
-
-        process.Start();
-
-        // Send the command to the command prompt
-        process.StandardInput.WriteLine(command);
-        process.StandardInput.Flush();
-        process.StandardInput.Close();
-
-        // Display the command prompt output
-        string output = process.StandardOutput.ReadToEnd();
-        Console.WriteLine(output);
-
-        process.WaitForExit();
-        process.Close();
+            Console.WriteLine($"Error executing command: {ex.Message}");
+        }
     }
 
-
-
-
-
+    // Function to create a project ID
     static string MakeProjectId(string projectName)
     {
         // Remove spaces, convert to lowercase, and replace spaces with hyphens
